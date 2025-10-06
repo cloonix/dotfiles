3106edc9847d376460928684142cdc2ead4662de
Claus Malter <gpg@cmmx.de>
2025-10-06 16:50:30 +0200

    disable temporary files in vim and neovim

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,26 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Security: Disable temporary files for sensitive data (gopass, etc.)
+-- neovim on Linux
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = "/dev/shm/gopass*",
+  callback = function()
+    vim.opt_local.swapfile = false
+    vim.opt_local.backup = false
+    vim.opt_local.undofile = false
+    vim.opt_local.shada = ""
+  end,
+})
+
+-- neovim on MacOS
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = "/private/**/gopass**",
+  callback = function()
+    vim.opt_local.swapfile = false
+    vim.opt_local.backup = false
+    vim.opt_local.undofile = false
+    vim.opt_local.shada = ""
+  end,
+})
