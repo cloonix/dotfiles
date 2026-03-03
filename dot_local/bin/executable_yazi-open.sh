3ef844946a68b92becea7d9be6c8b69b5c546dbe
Claus Malter <3120297+cloonix@users.noreply.github.com>
2026-03-03 18:25:18 +0100

    focus fix

--- dot_local/bin/executable_yazi-open.sh
+++ dot_local/bin/executable_yazi-open.sh
@@ -4,8 +4,7 @@ FOLDER="${1:-$HOME}"
 
 if pgrep -x yazi >/dev/null 2>&1; then
   ya emit-to "$MY_ID" tab_create "$FOLDER"
+  osascript -e 'tell application "System Events" to set frontmost of process "kitty" to true'
 else
   kitty yazi --client-id "$MY_ID" "$FOLDER" &
 fi
-
-osascript -e 'tell application "kitty" to activate'
