db423159565617b38798359e029dc7795e61919a
Claus Malter <3120297+cloonix@users.noreply.github.com>
2026-03-03 17:28:53 +0100

    yazi open wrapper for macos

---
+++ dot_local/bin/executable_yazi-open.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+MY_ID=12345678
+FOLDER="${1:-$HOME}"
+
+if pgrep -x yazi >/dev/null 2>&1; then
+  ya emit-to "$MY_ID" tab_create "$FOLDER"
+else
+  kitty yazi --client-id "$MY_ID" "$FOLDER" &
+fi
