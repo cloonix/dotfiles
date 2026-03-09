f606c2bcee3a1c31b7beb8d4e25782c80aa8b842
Claus Malter <3120297+cloonix@users.noreply.github.com>
2026-03-09 13:21:45 +0100

    yazi

--- dot_local/bin/executable_yazi-open.sh
+++ dot_local/bin/executable_yazi-open.sh
@@ -2,9 +2,10 @@
 MY_ID=12345678
 FOLDER="${1:-$HOME}"
 
-if pgrep -x yazi >/dev/null 2>&1; then
-  ya emit-to "$MY_ID" tab_create "$FOLDER"
+if ya emit-to "$MY_ID" tab_create "$FOLDER" 2>/dev/null; then
+  # emit succeeded → instance with that ID is running
   osascript -e 'tell application "System Events" to set frontmost of process "kitty" to true'
 else
+  # emit failed → no instance with that ID, launch one
   kitty yazi --client-id "$MY_ID" "$FOLDER" &
 fi
